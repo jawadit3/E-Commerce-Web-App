bae28430b6a960234d79f570b20a9ddf95acc1d9
jawad127 <jawadit3@gmail.com>
2022-12-28 11:34:35 +0500

    delete-products completed

--- Project/src/app/seller-home/seller-home.component.ts
+++ Project/src/app/seller-home/seller-home.component.ts
@@ -10,13 +10,36 @@ import { Product } from '../types/Product';
 export class SellerHomeComponent implements OnInit {
 
   productList : Product[] | undefined;
+  message : String | undefined;
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+   this.loadProducts();
+  }
+
+  deleteProduct(id: number | undefined){
+    if(id){
+      this.productService.deleteProduct(id).subscribe(
+        data => {
+          if(data){
+            this.message = "product removed.";
+            this.loadProducts();
+            this.setMessage();
+
+          }
+        }
+      );
+    }
+  }
+
+  loadProducts(){
     this.productService.getProducts()
     .subscribe(data => {
       this.productList = data;
     });
   }
 
+  setMessage(){
+    setTimeout(() => this.message = undefined, 3000);
+  }
 }
--- Project/src/app/services/product.service.ts
+++ Project/src/app/services/product.service.ts
@@ -23,4 +23,8 @@ export class ProductService {
   getProducts(): Observable<Product[]>{
     return this.http.get<Product[]>("http://localhost:3000/products");
   }
+
+  deleteProduct(id: number | undefined){
+      return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
 }
